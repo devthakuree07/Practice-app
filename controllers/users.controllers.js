c1f1d39a23891f03d97b28c9abd85caf538b1caf
dev <idfkdev@gmail.com>
2025-06-02 23:54:56 +0545

    Worked on login and register functionality of a user

--- controllers/users.controllers.js
+++ controllers/users.controllers.js
@@ -1,9 +1,43 @@
 const { User } = require("../models/users");
 
 const testHome = (req, res) => {
-    return res.send(" Home Page of Practice-app!");
+  return res.send(" Home Page of Practice-app!");
 };
 
-module.exports = { testHome };
+const registerUser = async (req, res) => {
+  const { username, email, password } = req.body;
 
+  const existingUser = await User.findOne({ where: { email } });
+  if (existingUser) {
+    return res.json("User already exists!");
+  }
 
+  const createUser = await User.create({ username, email, password });
+  if (!createUser) {
+    return res.json("Invalid credentials!");
+  } else {
+    return res.json("User registered successfully!");
+  }
+};
+
+const loginUser = async (req, res) => {
+    const { username, email, password } = req.body;
+
+    const existingUser =  await User.findOne({ where: {email} });
+    if(!existingUser){
+        return res.json("User doesn't exists!");
+    }
+
+    if(!password === existingUser.password) {
+        return res.json("Invalid email or password!");
+    }
+
+    return res.json("User login successfully!", { data: {
+        username,
+        email
+    }});
+    
+
+};
+
+module.exports = { testHome, registerUser, loginUser };
--- routes/users.routes.js
+++ routes/users.routes.js
@@ -1,8 +1,10 @@
 const express = require("express");
-const { testHome } = require("../controllers/users.controllers");
+const { testHome, registerUser, loginUser } = require("../controllers/users.controllers");
 
 const userRouter = express.Router();
 
 userRouter.get('/', testHome); 
+userRouter.post('/register', registerUser); 
+userRouter.post('/login', loginUser); 
 
 module.exports = { userRouter };
